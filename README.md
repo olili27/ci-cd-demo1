04af679587d33d1bc76e70e933bbac56d5038c68
olili27 <timmolili27@gmail.com>
2025-04-18 10:20:18 +0300

    created a django project
    this is to test and demo a ci/cd pipeline with jenkins.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Guide
+    
+    - create a virtual environment depending on the operating system you are using
+    - activate the virtual environment
+    - install the required packages using the requirements.txt
---
+++ greeting/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+# Create your tests here.
+class GreetingTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_page(self):
+        response = self.client.get(reverse("index"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello")
+        self.assertTemplateUsed(response, "index.html")
+
+
+class GreetingFunctionalityTests(TestCase):
+    def test_greeting(self):
+        response = self.client.get(reverse("index"))
+
+        self.assertEqual(response.context["greeting"], "Hello")
+        # self.assertContains(response, "Hello")
+        # self.assertTemplateUsed(response, "index.html")
